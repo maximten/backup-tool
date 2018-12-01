168a93364fe53e1b50e1ac990cfb92c5ab104363
Maxim Ten <maximten94@gmail.com>
2018-12-01 15:33:05 +0600

    init commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const yargs = require('yargs')
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
+const yaml = require('js-yaml')
+const validate = require('./lib/validate')
+const backup = require('./lib/backup')
+
+const argv = yargs.help('help')
+  .usage('Usage: node $0')
+  .version('0.0.1')
+  .option('file', {
+    alias: 'f',
+    description: 'Specify config file',
+    string: true,
+    default: 'backup-config.yml'
+  })
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .argv
+
+let config = null
+try {
+  const { file } = argv
+  const filePath = path.resolve(file)
+  const content = fs.readFileSync(filePath, 'utf8')
+  config = yaml.safeLoad(content)
+} catch (e) {
+  console.error(chalk.red(e.message))
+  process.exit(1)
+}
+
+try {
+  validate(config)
+} catch (e) {
+  console.error(chalk.red(e.message))
+  process.exit(1)
+}
+
+try {
+  backup(config)
+} catch (e) {
+  console.error(chalk.red(e.message))
+}
